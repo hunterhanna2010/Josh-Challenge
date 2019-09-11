49468d2a2cd49607a2b4813f8dce7ff77f4bab0b
Joshua Hunter <50775251+hunterhanna2010@users.noreply.github.com>
2019-09-11 16:32:50 -0700

    Fixed bugs
    Limited qb list to only top 3.
    Streamlined code
    H2 heading appended to correct section

--- app.js
+++ app.js
@@ -1,120 +1,143 @@
 //Instructions
-            //1: Create a list of the top 3 quarterbacks in football according to you
-            
-            /*to accomplish this task, I have an input field text box, but I need
-            to create a button element. Add a click event listener to the button. And then add
-            the value of the input text box to the html page. I will set up list structure and append it to the input value. This way someone could add dan marino or jo montana*/
-            let qbs = 0;
-            const button = document.createElement('BUTTON');
-            button.textContent = 'Add Your Top 3 QBs';
-            document.getElementById('qb').appendChild(button);
+//1: Create a list of the top 3 quarterbacks in football according to you
 
-            button.addEventListener('click', () => {
-                true;
-                let ul = document.getElementsByTagName('ul')[0];
-                let li =document.createElement('li');
-                li.textContent = addItemInput.value;
-                ul.appendChild(li);
-                attachListItemButton(li);
-                addItemInput.value = ' ';
-                console.log(li);
-                qbs++;
-                console.log(qbs);
-            })
-            
-        
-            //decided to add some functionality buttons to the dom so the user can adjust where their favorite qb should be
-            const listDiv = document.querySelector('.qb');
-            const listUl = listDiv.querySelector('ul');
-            const addItemInput = document.querySelector('input.addItemInput');
-            
-            function attachListItemButton(li) {
-                let up = document.createElement('button')
-                let down = document.createElement('button')
-                let remove = document.createElement('button')
-                
-                up.className = 'up';
-                up.textContent = 'Move QB UP';
-                li.appendChild(up);
-                
-                down.className = 'down';
-                down.textContent = 'Move QB DOWN';
-                li.appendChild(down);
-                
-                remove.className = 'remove';
-                remove.textContent = 'Take Him Out';
-                li.appendChild(remove);
-              }
-              
-              listUl.addEventListener('click', (event) => {
-                if (event.target.tagName == 'BUTTON') {
-                  if (event.target.className == 'remove') {
-                   let li = event.target.parentNode;
-                   let ul = li.parentNode;
-                   ul.removeChild(li);
-                  }
-                }
-            
-                  if (event.target.className == 'up') {
-                   let li = event.target.parentNode;
-                   let prevLi = li.previousElementSibling;
-                   let ul = li.parentNode;
-                   if(prevLi) { 
-                   ul.insertBefore(li, prevLi);
-                   }
-                  }  
-                   if (event.target.className == 'down') {
-                   let li = event.target.parentNode;
-                   let nextLi = li.nextElementSibling;
-                   let ul = li.parentNode;
-                   if(nextLi) { 
-                   ul.insertBefore(nextLi, li);
-                  }
-                }; 
+/*to accomplish this task, I have an input field text box, but I need
+to create a button element. Add a click event listener to the button. And then add
+the value of the input text box to the html page. I will set up list structure and append it to the input value. This way someone could add dan marino or jo montana*/
+let qbs = 0;
+const button = document.createElement('BUTTON');
+button.textContent = 'Add Your Top 3 QBs';
+document.getElementById('qb').appendChild(button);
 
-              });
-                
-              //trying to remove event listener from click so users can't add more than 3 qbs
-              /*for(let i = 0; i <= li.length; i++) {
-                if (qbs > 3) {
-                    button.removeEventListener('click', () => {
-                    let p = document.textContent = 'too many qbs';   
-                    })*/
-                
-                //2: Rearrange the best running backs in the league to reverse their order
-                //get list items as a node list
-            const rbList = document.querySelector('#rbList');
-                //convert ul items to an array
-            let rbArray = Array.prototype.slice.call(rbList);
-            rbArray.push(' Alvin Kamara', ' Saquon Barkley', ' Ezekiel Elliott');
-            console.log(rbArray);
-            
-                //reverse order of rbArray
-            rbArray.reverse();
-            console.log(rbArray);
-                //supply text to the DOM
-            rbList.textContent = "Here is the reversed order of Running Backs: " + rbArray;
-            console.log(rbList);
-            let odds = document.getElementsByClassName('odds');
-            let h2Heading = document.createElement('h2');
-            h2Heading.textContent = 'Vegas Odds For Winning the SuperBowl';
-            //h2Heading.appendChild(h2Heading);
-            document.body.appendChild(h2Heading);
-            //document.getElementById("odds").appendChild(h2Heading);  
-            //3: Find Vegas odds online for the three superbowl favorites and include these odds next to the list
-            //New England Patriots 7/1
-            let patriots = document.getElementById('newEngland').textContent = '7/1, Patriots';
-            //Kansas City Chiefs 8/1
-            let chiefs = document.getElementById('kansasCity').textContent = '8/1, Chiefs';
-            //New Orleans Saints 9/1
-            let saints = document.getElementById('newOrleans').textContent = '9/1, Saints';         
-            
-            //4: Change the color of each entry on the page to reflect the team represented
-            let patriotsColor = document.getElementById('blue').style.backgroundColor = '#002244';
-            patriotsColor += document.getElementById('blue').style.color = '#B0B7BC';
+button.addEventListener('click', () => {
+    // only run if qbs < 3
+    if (qbs < 3) { 
+      let ul = document.getElementById('qbList');
+      let li = document.createElement('li');
+      li.textContent = addItemInput.value;
+      ul.appendChild(li);
+      attachListItemButton(li);
+      addItemInput.value = '';
+      console.log(li);
+      qbs++;
+      console.log(qbs);
+    }
+})
 
-            let chiefsColor = document.getElementById('red').style.backgroundColor = '#E31837';
-            chiefsColor += document.getElementById('red').style.color = '#FFB81C';
 
-            let saintsColor = document.getElementById('black').style.backgroundColor = '#101820';
-            saintsColor += document.getElementById('black').style.color = '#D3BC8D';
+//decided to add some functionality buttons to the dom so the user can adjust where their favorite qb should be
+const listDiv = document.querySelector('.qb');
+const listUl = listDiv.querySelector('ul');
+const addItemInput = document.querySelector('input.addItemInput');
+
+function attachListItemButton(li) {
+    let up = document.createElement('button')
+    let down = document.createElement('button')
+    let remove = document.createElement('button')
+    
+    up.className = 'up';
+    up.textContent = 'Move QB UP';
+    li.appendChild(up);
+    
+    down.className = 'down';
+    down.textContent = 'Move QB DOWN';
+    li.appendChild(down);
+    
+    remove.className = 'remove';
+    remove.textContent = 'Take Him Out';
+    li.appendChild(remove);
+  }
+  
+  listUl.addEventListener('click', (event) => {
+    if (event.target.tagName == 'BUTTON') {
+      if (event.target.className == 'remove') {
+        let li = event.target.parentNode;
+        let ul = li.parentNode;
+        ul.removeChild(li);
+        qbs--;
+      }
+    }
+
+    if (event.target.className == 'up') {
+      let li = event.target.parentNode;
+      let prevLi = li.previousElementSibling;
+      let ul = li.parentNode;
+      if(prevLi) { 
+        ul.insertBefore(li, prevLi);
+      }
+    }  
+    
+    if (event.target.className == 'down') {
+      let li = event.target.parentNode;
+      let nextLi = li.nextElementSibling;
+      let ul = li.parentNode;
+      if(nextLi) { 
+        ul.insertBefore(nextLi, li);
+      }
+    }; 
+  });
+    
+  //trying to remove event listener from click so users can't add more than 3 qbs
+  /*for(let i = 0; i <= li.length; i++) {
+    if (qbs > 3) {
+        button.removeEventListener('click', () => {
+        let p = document.textContent = 'too many qbs';   
+        })*/
+    
+    //2: Rearrange the best running backs in the league to reverse their order
+    //get list items as a node list
+    const rbListItems = document.querySelectorAll('#rbList li');
+    const rbList = document.querySelector('#rbList');
+console.log(rbList)
+
+//convert ul items to an array
+let rbArray = []
+for (let i = 0; i < rbListItems.length; i++) {
+  rbArray.push(rbListItems[i].textContent)
+}
+console.log(rbArray);
+
+    //reverse order of rbArray
+rbArray.reverse();
+console.log(rbArray);
+
+//supply text to the DOM
+for (let i = 0; i < rbArray.length; i++) {
+  let newLi = document.createElement('li');
+  newLi.textContent = rbArray[i]
+  rbList.appendChild(newLi)
+}
+
+console.log(rbList);
+
+
+
+//let odds = document.getElementsByClassName('odds');
+function oddsMessage () {
+    let odds = document.getElementById('odds')
+    odds.textContent = 'SuperBowl Odds';
+    //odds.insertAdjacentHTML("afterbegin", `<h2><strong>Vegas SuperBowl Odds</strong></h2>`);
+
+}
+oddsMessage();
+
+//h2Heading.appendChild(h2Heading);
+//odds = document.body.appendChild(H2);
+//document.getElementById("odds").appendChild(h2Heading);  
+//3: Find Vegas odds online for the three superbowl favorites and include these odds next to the list
+//New England Patriots 7/1
+let patriots = document.getElementById('newEngland').textContent = '7/1, Patriots';
+//Kansas City Chiefs 8/1
+let chiefs = document.getElementById('kansasCity').textContent = '8/1, Chiefs';
+//New Orleans Saints 9/1
+let saints = document.getElementById('newOrleans').textContent = '9/1, Saints';         
+
+//4: Change the color of each entry on the page to reflect the team represented
+let patriotsColor = document.getElementById('blue').style.backgroundColor = '#002244';
+patriotsColor += document.getElementById('blue').style.color = '#B0B7BC';
+
+let chiefsColor = document.getElementById('red').style.backgroundColor = '#E31837';
+chiefsColor += document.getElementById('red').style.color = '#FFB81C';
+
+let saintsColor = document.getElementById('black').style.backgroundColor = '#101820';
+saintsColor += document.getElementById('black').style.color = '#D3BC8D';
